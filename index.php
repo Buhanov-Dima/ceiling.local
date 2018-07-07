f766ac2774f822aceaa9f9e1051cc13fba74c99a
Buhanov-Dima <35217770+Buhanov-Dima@users.noreply.github.com>
2018-07-07 22:42:50 +0500

    2block

---
+++ index.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Seiling</title>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="/css/owl-carousel/owl.carousel.min.css">
+	<link rel="stylesheet" href="/css/owl-carousel/owl.theme.default.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.css" />
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800&amp;subset=cyrillic-ext" rel="stylesheet">
+	<link rel="stylesheet" href="/css/style.css">
+	<link rel="stylesheet" href="css/style-lg.css" media="only screen and (max-width:1200px)">
+	<link rel="stylesheet" href="css/style-xl.css" media="only screen and (max-width:992px)">
+	<link rel="stylesheet" href="css/style-md.css" media="only screen and (max-width:768px)">
+	<link rel="stylesheet" href="css/style-xs.css" media="only screen and (max-width:576px)">
+</head>
+<body>
+	
+<section id="b1">
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<div class="wrap-b1">
+					<div class="wrapper_phone_b1">
+						<div class="logo">
+							<img src="/img/logo.png" alt="">
+							<p>ПРОИЗВОДСТВО, ПРОДАЖА И МОНТАЖ НАТЯЖНЫХ ПОТОЛКОВ</p>
+						</div>
+						<div class="phone__callback">
+							<a href="#" class="phone">+ 7 (495) 021-72-73</a>
+							<button class="btn-popup">ОБРАТНЫЙ ЗВОНОК</button>
+						</div>
+					</div>
+					<div class="wrapper__main_b1">
+						<h3 class="standart-h">СКИДКА 5%</h3>
+						<p>при заключении договора в день замера</p>
+						<button class="btn-main">РАССЧИТАТЬ ПРОЕКТ СО СКИДКОЙ</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<header>
+		<nav class="navbar navbar-expand-lg navbar-light">
+		  <a class="navbar-brand" href="#"><img src="/img/logo.png" alt=""></a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+
+		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+		    <ul class="navbar-nav ml-auto">
+		      <li>
+		      	<a class="nav-link g-token" href="#">Get tokens</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">WHITEPAPER</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">ABOUT</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">TEAM</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">F.A.Q.</a>
+		      </li>
+		      <li class="nav-item dropdown">
+		        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+		          ENG
+		        </a>
+		        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+		          <a class="dropdown-item" href="#">RUS</a>
+		        </div>
+		      </li>
+		    </ul>
+		  </div>
+		</nav>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-xl-12">
+					<div class="top-menu">
+						<a href="#" class="active">О КОМПАНИИ</a>
+						<a href="#">ВИДЫ ПОТОЛКОВ</a>
+						<a href="#">КАТАЛОГ ЦВЕТОВ</a>
+						<a href="#">ЭТАПЫ РАБОТЫ</a>
+						<a href="#">ВАШИ ОТЗЫВЫ</a>
+						<a href="#">КОНТАКТЫ</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<section id="b2">
+		<div class="container-fluid pdg-0">
+			<div class="row">
+				<div class="col-xl-12">
+					<div class="wrap-b2">
+						<h3 class="standart-h text-center">ПОТОЛКИ<span>.STUDIO</span></h3>
+						<div class="b2-uslugi">
+							<div class="b2-bg-l">
+								
+							</div>
+							<div class="uslugi-txt">
+								<h5>Работа с дизайн-проектами любой сложности</h5>
+								<p>Благодаря опыту и высокой квалификации нашей команды</p>
+							</div>
+							<div class="b2-bg-r">
+								
+							</div>
+						</div>
+						<div class="b2-uslugi">
+							<div class="b2-bg-l bg-2">
+								
+							</div>
+							<div class="uslugi-txt">
+								<h5>Собственное производство</h5>
+								<p>Позволяет в кратчайшие сроки произвести любой материал</p>
+							</div>
+							<div class="b2-bg-r bg-2">
+								
+							</div>
+						</div>
+						<div class="b2-uslugi">
+							<div class="b2-bg-l bg-3">
+								
+							</div>
+							<div class="uslugi-txt">
+								<h5>15 лет гарантия на материалы</h5>
+								<p>Производство соответствует мировым стандартам качества</p>
+							</div>
+							<div class="b2-bg-r bg-3">
+								
+							</div>
+						</div>
+						<div class="b2-uslugi">
+							<div class="b2-bg-l bg-4">
+								
+							</div>
+							<div class="uslugi-txt">
+								<h5>2 года гарантия на монтаж</h5>
+								<p>Ведь к работе допускаются только квалифицированные специалисты</p>
+							</div>
+							<div class="b2-bg-r bg-4">
+								
+							</div>
+						</div>
+						<div class="b2-uslugi">
+							<div class="b2-bg-l bg-5">
+								
+							</div>
+							<div class="uslugi-txt">
+								<h5>Низкие цены на материалы</h5>
+								<p>Все потому, что Вы не переплачиваете за работу диллеров и посредников</p>
+							</div>
+							<div class="b2-bg-r bg-5">
+								
+							</div>
+						</div>
+						<div class="b2-uslugi">
+							<div class="b2-bg-l bg-6">
+								
+							</div>
+							<div class="uslugi-txt">
+								<h5>Монтаж "Без пыли"</h5>
+								<p>При монтаже используются перфораторы с пылесборником</p>
+							</div>
+							<div class="b2-bg-r bg-6">
+								
+							</div>
+						</div>
+						<div class="text-center">
+							<button class="btn-main">РАССЧИТАТЬ СТОИМОСТЬ</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	<script src="/js/owl.carousel.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.js"></script>
+	<script src="/js/main.js"></script>
+</body>
+</html>
