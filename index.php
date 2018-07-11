6941f28a57a3dbb24e9583abf916bd466875b4be
DmitriOkulov <dmitriokulov@yandex.ru>
2018-07-11 21:23:20 +0500

    fx

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 <head>
-	<title>Seiling</title>
+	<title>Потолки Studio</title>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
